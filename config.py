a0922caea9ee54c062730aa7b23df7e53b7b5e2d
mohan9638 <158275364+mohan9638@users.noreply.github.com>
2025-06-01 21:46:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "25693368"))
+API_HASH = environ.get("API_HASH", "2dcf91b0f99c0b9d4875e87020e6bd07")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 MONGO_URI = environ.get("MONGO_URI", "")
-OWNER_ID = int(environ.get("OWNER_ID", ""))
+OWNER_ID = int(environ.get("OWNER_ID", "5927517339"))
